c1694b486ed6d8131cea3234b85c62bf9be42338
legalize <unknown>
2010-02-02 06:16:49 +0000

    Implemented : Update test

--- RefactorTest/ReplaceWithLocal.cpp
+++ RefactorTest/ReplaceWithLocal.cpp
@@ -6,17 +6,19 @@
 
 namespace ReplaceWithLocalNamespace
 {
-	void TestString()
+	void Test()
 	{
 		std::string temp = "This is a temp string.";
 
 		std::string foo = temp;
 		foo += "  This is more text.";
+		std::string bar(temp + "  Yet even more!");
 		// #TEST#: RWL1 Replace With Local on temp
-		foo += temp;
+		foo += temp + "  Yet even more!";
 	}
 }
 
 void TestReplaceWithLocal()
 {
+	ReplaceWithLocalNamespace::Test();
 }
