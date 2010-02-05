807b71661c0f0cd03a9acd470a3e49fe6346f2d6
legalize <unknown>
2010-02-05 02:20:24 +0000

    Implemented : Handle multiple results files

--- TestResults.js
+++ TestResults.js
@@ -1,12 +1,7 @@
 // Print out test results
-var fso = new ActiveXObject("Scripting.FileSystemObject");
-
-var infile = fso.OpenTextFile("RefactorProResults.txt");
 var refactoring;
 var passes;
 var failures;
-var totalPasses = 0;
-var totalFailures = 0;
 
 function Percentage(passed, total)
 {
@@ -21,44 +16,57 @@ function StartTest()
 	failures = 0;
 }
 
-StartTest();
-
-var passedAllTests = new Array();
-
-while (!infile.AtEndOfStream)
+function main(resultsFile)
 {
-	var line = infile.ReadLine();
-	if (line.length == 0)
-	{
-		if (passes + failures > 0)
-		{
-			if (failures == 0)
-			{
-				passedAllTests.push(refactoring);
-			}
-			WScript.Echo(Percentage(passes, passes + failures) + refactoring + ": " + passes + " passed, " + failures + " failed");
-			totalPasses += passes;
-			totalFailures += failures;
-		}
-		StartTest();
-	}
-	else if (line.indexOf("Pass") > 0)
-	{
-		passes++;
-	}
-	else if (line.indexOf("Failure") > 0)
-	{
-		failures++;
-	}
-	else if (line.indexOf("= ") == 0)
-	{
-		refactoring = line.substr(line.indexOf("= ") + 1);
-	}
-}
+	StartTest();
 
-WScript.Echo("");
-WScript.Echo(passedAllTests.length + " Passed all tests:");
-WScript.Echo(passedAllTests.join("\n"));
-WScript.Echo("");
-var numTests = totalPasses + totalFailures;
-WScript.Echo("Total: " + Percentage(totalPasses, numTests) + " (" + totalPasses + " passed + " + totalFailures + " failed = " + numTests + " total)");
+	var fso = new ActiveXObject("Scripting.FileSystemObject");
+	var infile = fso.OpenTextFile(resultsFile);
+	var totalPasses = 0;
+	var totalFailures = 0;
+	var passedAllTests = new Array();
+	while (!infile.AtEndOfStream)
+	{
+		var line = infile.ReadLine();
+		if (line.length == 0)
+		{
+			if (passes + failures > 0)
+			{
+				if (failures == 0)
+				{
+					passedAllTests.push(refactoring);
+				}
+				WScript.Echo(Percentage(passes, passes + failures) + refactoring + ": " + passes + " passed, " + failures + " failed");
+				totalPasses += passes;
+				totalFailures += failures;
+			}
+			StartTest();
+		}
+		else if (line.indexOf("Pass") > 0)
+		{
+			passes++;
+		}
+		else if (line.indexOf("Failure") > 0)
+		{
+			failures++;
+		}
+		else if (line.indexOf("= ") == 0)
+		{
+			refactoring = line.substr(line.indexOf("= ") + 1);
+		}
+	}
+
+	WScript.Echo("");
+	WScript.Echo(passedAllTests.length + " Passed all tests:");
+	WScript.Echo(passedAllTests.join("\n"));
+	WScript.Echo("");
+	var numTests = totalPasses + totalFailures;
+	WScript.Echo("Total: " + Percentage(totalPasses, numTests) + " (" + totalPasses + " passed + " + totalFailures + " failed = " + numTests + " total)");}
+
+var resultsFile = "RefactorProResults.txt";
+if (WScript.Arguments.Length > 0)
+{
+	resultsFile = WScript.Arguments(0);
+}
+WScript.Echo("Results file: " + resultsFile);
+main(resultsFile);
