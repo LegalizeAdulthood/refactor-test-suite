5d105e8e92864b58234c1ac8c559b00453c4e6e8
legalize <unknown>
2010-02-06 00:00:19 +0000

    Implemented : Add tests

--- RefactorTest/ChangeSignature.cpp
+++ RefactorTest/ChangeSignature.cpp
@@ -1,10 +1,18 @@
 #include "stdafx.h"
 #include "ChangeSignature.h"
+#include <cassert>
 
 namespace ChangeSignatureNamespace
 {
+	int Function2(int a, int b)
+	{
+		return a + b;
+	}
+
 	static void Test()
 	{
+		assert(7 == Function1(3, 4));
+		assert(7 == Function2(3, 4));
 	}
 }
 
