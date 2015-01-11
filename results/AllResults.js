29c7344bf5979b4b5535dd5237490fe55f2a76ff
Richard <legalize@xmission.com>
2015-01-10 21:51:33 -0700

    Compute markdown for all results

--- results/AllResults.js
+++ results/AllResults.js
@@ -62,19 +62,19 @@ function ProcessLine(context, line)
     {
         context.version = matches[1];
     }
-    else if (matches = line.match(/^Name: *([^ ].*)$/i))
+    else if (matches = line.match(/^Home: *\[([^\]]*)\]/i))
     {
         context.name = matches[1];
     }
-    else if (matches = line.match(/^([A-Z][A-Z]*[0-9][0-9]*)\s\s*Pass(.*)$/))
+    else if (matches = line.match(/^([A-Z][A-Z0-9]*)[^0-9]*Pass(.*)$/))
     {
         testResult(ctx.passes, matches);
     }
-    else if (matches = line.match(/^([A-Z][A-Z]*[0-9][0-9]*)\s\s*Failure(.*)$/))
+    else if (matches = line.match(/^([A-Z][A-Z0-9]*)[^0-9]*Failure(.*)$/))
     {
         testResult(ctx.failures, matches);
     }
-    else if (line.indexOf("= ") == 0)
+    else if (line.indexOf("## ") == 0)
     {
         ctx.refactoring = line.substr(2);
     }
@@ -100,27 +100,6 @@ function ProcessResultsFile(resultsFile, next)
         });
 }
 
-function ProcessResults(dir, next)
-{
-    var results = [];
-    fs.readdir(dir, function(err, files) {
-    async.eachSeries(files,
-        function(file, nextFile) {
-            if (file.match(/^[A-Z].*Results\.txt$/)) {
-                ProcessResultsFile(file, function(err, fileResults) {
-                    results.push(fileResults);
-                    nextFile(null);
-                });
-            } else {
-                nextFile(null);
-            }
-        },
-        function(err) {
-            next(err, results);
-        });
-    });
-}
-
 function testData(tests, id) {
     var key = (typeof(id) === "string") ? id : id[0];
 
@@ -130,29 +109,88 @@ function testData(tests, id) {
     return tests[key];
 }
 
+function CompareIds(id1, id2)
+{
+    var parts1 = id1.match(/^(.*[A-Z])([0-9]+)$/),
+        parts2 = id2.match(/^(.*[A-Z])([0-9]+)$/),
+        label1 = parts1[1],
+        label2 = parts2[1],
+        num1 = Number(parts1[2]),
+        num2 = Number(parts2[2]);
+
+    if (label1 === label2)
+    {
+        return (num1 < num2) ? -1 : ((num1 === num2) ? 0 : 1);
+    }
+    return (label1 < label2) ? -1 : 1;
+}
+
+function ResultsMarkdown(tests)
+{
+    //console.log(JSON.stringify(tests, null, 4));
+    console.log("# Overall Results");
+    console.log("");
+    console.log("Case | Passed | Failed");
+    console.log("---- | ------ | ------");
+    _.each(Object.keys(tests).sort(CompareIds), function(caseId) {
+        console.log(caseId + " | " + tests[caseId].passes.join(", ") + " | " + tests[caseId].failures.join(", "));
+    });
+}
+
 function Summarize(err, results)
 {
     var tests = {};
     _.map(results, function(product) {
         _.map(product.tests, function(test) {
             _.map(test.passes, function(id) {
-                testData(tests, id).passes.push(product.name);
+                testData(tests, id).passes.push(product.name + " " + product.version);
             });
             _.map(test.failures, function(id) {
-                testData(tests, id).failures.push(product.name);
+                testData(tests, id).failures.push(product.name + " " + product.version);
             });
         });
     });
-    console.log(JSON.stringify(tests, null, 4));
+    ResultsMarkdown(tests);
+}
+
+function ProcessFiles(files, next)
+{
+    var results = [];
+    async.eachSeries(files,
+        function(file, nextFile) {
+            ProcessResultsFile(file, function(err, fileResults) {
+                results.push(fileResults);
+                nextFile(null);
+            });
+        },
+        function(err) {
+            next(err, results);
+        });
+}
+
+function ProcessResults(dir, next)
+{
+    fs.readdir(dir, function(err, files) {
+        if (err) {
+            next(err);
+        } else {
+            ProcessFiles(_.filter(files, function(file) {
+                    return file.match(/^[A-Z].*Results\.md$/);
+                }), next);
+        }
+    });
 }
 
 function main(argv) {
-    var resultsFile = "RefactorProResults.txt";
+    var resultsFile = "RefactorProResults.md";
     if (argv.length > 2)
     {
-        resultsFile = argv[2];
+        ProcessFiles(argv.slice(2), Summarize);
     }
-    ProcessResults(".", Summarize);
+    else
+    {
+        ProcessResults(".", Summarize);
+    }
 }
 
 main(process.argv);
