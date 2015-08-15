7f6447fbbfd6d7e87dffb6ae5b18cacfe6b8b763
Richard <legalize@xmission.com>
2015-08-15 17:07:41 -0600

    Implemented: Add more code for testing move implementation to source file

--- RefactorTest/MoveImplementationToSourceFile.cpp
+++ RefactorTest/MoveImplementationToSourceFile.cpp
@@ -1,13 +1,27 @@
 #include "stdafx.h"
 #include "MoveImplementationToSourceFile.h"
-#include "Require.h"
 
 void TestMoveImplementationToSourceFile()
 {
     using namespace MoveImplementationToSourceFile;
 
-    require_equal(1, f());
+    REQUIRE_EQUAL(1, f());
 
     Base b;
-    require_equal(1, b.f());
+    REQUIRE_EQUAL(1, b.f());
+    b.g();
+
+    Variant v;
+    v.i = 1;
+    REQUIRE_EQUAL(1, v.getInt());
+
+    Derived d;
+    REQUIRE_EQUAL(1, d.h());
+    int (VirtualBase::*p)() const = d.GetOperation();
+    REQUIRE_EQUAL(1, (d.*p)());
+
+    Nested::Derived nd;
+    REQUIRE_EQUAL(1, nd.h());
+    int (Nested::VirtualBase::*np)() const = nd.GetOperation();
+    REQUIRE_EQUAL(1, (nd.*np)());
 }
--- RefactorTest/MoveImplementationToSourceFile.h
+++ RefactorTest/MoveImplementationToSourceFile.h
@@ -1,8 +1,10 @@
 #pragma once
+#include "Require.h"
 
 namespace MoveImplementationToSourceFile
 {
 
+// #TEST#: MISF1 Move implementation to source file
 inline int f()
 {
     return 1;
@@ -10,13 +12,128 @@ inline int f()
 
 struct Base
 {
+    Base() : i{10}
+    {}
+
+    ~Base()
+    {}
+
+    // #TEST#: MISF2 Move implementation to source file
     int f()
     {
         return 1;
     }
 
+    // #TEST#: MISF3 Move implementation to source file
+    void g()
+    {
+        REQUIRE_EQUAL(1, f());
+    }
 
+private:
+    int i;
 };
+
+union Variant
+{
+    int i;
+    float f;
+    double d;
+
+    int getInt() const
+    {
+        return i;
+    }
+};
+
+class VirtualBase
+{
+public:
+    virtual ~VirtualBase() {}
+
+    virtual void f() = 0;
+
+    virtual void g()
+    {
+    }
+
+    virtual int h() const = 0;
+
+    virtual int (VirtualBase::*GetOperation() const)() const = 0;
+};
+
+class Derived : public VirtualBase
+{
+public:
+    Derived()
+    {}
+
+    virtual ~Derived()
+    {}
+
+    void f() override
+    {}
+
+    void g() override
+    {}
+
+    int h() const override
+    {
+        return 1;
+    }
+
+    int (VirtualBase::*GetOperation() const)() const override
+    {
+        return &VirtualBase::h;
+    }
+};
+
+namespace Nested
+{
+
+class VirtualBase
+{
+public:
+    virtual ~VirtualBase() {}
+
+    virtual void f() = 0;
+
+    virtual void g()
+    {
+    }
+
+    virtual int h() const = 0;
+
+    virtual int (VirtualBase::*GetOperation() const)() const = 0;
+};
+
+class Derived : public VirtualBase
+{
+public:
+    Derived()
+    {}
+
+    virtual ~Derived()
+    {}
+
+    void f() override
+    {}
+
+    void g() override
+    {}
+
+    int h() const override
+    {
+        return 1;
+    }
+
+    int (VirtualBase::*GetOperation() const)() const override
+    {
+        return &VirtualBase::h;
+    }
+};
+
+}
 
 }
 
