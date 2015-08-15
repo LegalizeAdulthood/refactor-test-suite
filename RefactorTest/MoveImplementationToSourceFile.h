c319593de88442247d0f521b4d4b5b4a1f2efff3
Richard <legalize@xmission.com>
2015-08-15 17:13:05 -0600

    Implemented: Add tests for move implementation to source file

--- RefactorTest/MoveImplementationToSourceFile.h
+++ RefactorTest/MoveImplementationToSourceFile.h
@@ -12,19 +12,21 @@ inline int f()
 
 struct Base
 {
+    // #TEST#: MISF2 Move implementation to source file
     Base() : i{10}
     {}
 
+    // #TEST#: MISF3 Move implementation to source file
     ~Base()
     {}
 
-    // #TEST#: MISF2 Move implementation to source file
+    // #TEST#: MISF4 Move implementation to source file
     int f()
     {
         return 1;
     }
 
-    // #TEST#: MISF3 Move implementation to source file
+    // #TEST#: MISF5 Move implementation to source file
     void g()
     {
         REQUIRE_EQUAL(1, f());
@@ -40,6 +42,7 @@ union Variant
     float f;
     double d;
 
+    // #TEST#: MISF6 Move implementation to source file
     int getInt() const
     {
         return i;
@@ -49,39 +52,49 @@ union Variant
 class VirtualBase
 {
 public:
+    // #TEST#: MISF7 Move implementation to source file
     virtual ~VirtualBase() {}
 
+    // #TEST#: MISF8 Move implementation to source file
     virtual void f() = 0;
 
+    // #TEST#: MISF9 Move implementation to source file
     virtual void g()
     {
     }
 
+    // #TEST#: MISF10 Move implementation to source file
     virtual int h() const = 0;
 
+    // #TEST#: MISF11 Move implementation to source file
     virtual int (VirtualBase::*GetOperation() const)() const = 0;
 };
 
 class Derived : public VirtualBase
 {
 public:
+    // #TEST#: MISF12 Move implementation to source file
     Derived()
     {}
 
+    // #TEST#: MISF13 Move implementation to source file
     virtual ~Derived()
     {}
 
+    // #TEST#: MISF14 Move implementation to source file
     void f() override
     {}
 
     void g() override
     {}
 
+    // #TEST#: MISF15 Move implementation to source file
     int h() const override
     {
         return 1;
     }
 
+    // #TEST#: MISF16 Move implementation to source file
     int (VirtualBase::*GetOperation() const)() const override
     {
         return &VirtualBase::h;
@@ -94,39 +107,49 @@ namespace Nested
 class VirtualBase
 {
 public:
+    // #TEST#: MISF17 Move implementation to source file
     virtual ~VirtualBase() {}
 
+    // #TEST#: MISF18 Move implementation to source file
     virtual void f() = 0;
 
+    // #TEST#: MISF19 Move implementation to source file
     virtual void g()
     {
     }
 
+    // #TEST#: MISF20 Move implementation to source file
     virtual int h() const = 0;
 
+    // #TEST#: MISF21 Move implementation to source file
     virtual int (VirtualBase::*GetOperation() const)() const = 0;
 };
 
 class Derived : public VirtualBase
 {
 public:
+    // #TEST#: MISF22 Move implementation to source file
     Derived()
     {}
 
+    // #TEST#: MISF23 Move implementation to source file
     virtual ~Derived()
     {}
 
+    // #TEST#: MISF24 Move implementation to source file
     void f() override
     {}
 
     void g() override
     {}
 
+    // #TEST#: MISF25 Move implementation to source file
     int h() const override
     {
         return 1;
     }
 
+    // #TEST#: MISF26 Move implementation to source file
     int (VirtualBase::*GetOperation() const)() const override
     {
         return &VirtualBase::h;
