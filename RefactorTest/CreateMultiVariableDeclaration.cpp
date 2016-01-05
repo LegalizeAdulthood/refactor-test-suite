4fdb0b143c6f896a2ae5c8724618fc20e7792d3e
Richard Thomson <rthomson@daz3d.com>
2016-01-04 17:26:34 -0700

    Refactor: Clarify tests

--- RefactorTest/CreateMultiVariableDeclaration.cpp
+++ RefactorTest/CreateMultiVariableDeclaration.cpp
@@ -57,36 +57,36 @@ using namespace CreateMultiVariableDeclarationNamespace;
 
 void TestCreateMultiVariableDeclaration()
 {
-    // #TEST#: CMVD1 Create multi-variable declaration
+    // #TEST#: CMVD1 Create multi-variable declaration from next two lines
     int i;
     int j;
     i = -1;
     j = -2;
 
-    // #TEST#: CMVD2 Create multi-variable declaration
+    // #TEST#: CMVD2 Create multi-variable declaration from next two lines
     static int si;
     static int sj;
     si = -3;
     sj = -4;
 
-    // #TEST#: CMVD3 Create multi-variable declaration
+    // #TEST#: CMVD3 Create multi-variable declaration from next two lines
     std::vector<int>::size_type sti;
     std::vector<int>::size_type stj;
     sti = 0;
     stj = sti;
 
-    // #TEST#: CMVD4 Create multi-variable declaration
+    // #TEST#: CMVD4 Create multi-variable declaration from next two lines
     std::vector<float> vi(3);
     std::vector<float> vj(4);
     vi = vj;
 
-    // #TEST#: CMVD5 Create multi-variable declaration
+    // #TEST#: CMVD5 Create multi-variable declaration from next two lines
     void (*fn1)(void);
     void (*fn2)(void);
     fn1 = Function1;
     fn2 = Function2;
 
-    // #TEST#: CMVD6 Create multi-variable declaration
+    // #TEST#: CMVD6 Create multi-variable declaration from next four lines
     class Foo f1;
     class Foo f2;
     Foo *f3;
@@ -95,24 +95,24 @@ void TestCreateMultiVariableDeclaration()
     delete f3;
     f4 = f2;
 
-    // #TEST#: CMVD7 Create multi-variable declaration
+    // #TEST#: CMVD7 Create multi-variable declaration from next two lines
     int (Foo::*member1)() = &Foo::Operation1;
     int (Foo::*member2)() = &Foo::Operation2;
 
-    // #TEST#: CMVD8 Create multi-variable declaration
+    // #TEST#: CMVD8 Create multi-variable declaration from next two lines
     int (Foo::*cmember1)() const = &Foo::Const1;
     int (Foo::*cmember2)() const = &Foo::Const2;
 
-    // #TEST#: CMVD9 Create multi-variable declaration
+    // #TEST#: CMVD9 Create multi-variable declaration from next two lines
     struct Bar s1;
     struct Bar *s2 = new Bar();
 
-    // #TEST#: CMVD10 Create multi-variable declaration
+    // #TEST#: CMVD10 Create multi-variable declaration from next two lines
     Bar &s3 = s1;
     Bar **s4;
     s4 = &s2;
 
-    // #TEST#: CMVD11 Create multi-variable declaration
+    // #TEST#: CMVD11 Create multi-variable declaration from next four lines
     union Jumble j1;
     union Jumble j2;
     union Jumble j3;
@@ -122,7 +122,7 @@ void TestCreateMultiVariableDeclaration()
     j3._i = -3;
     j4 = &j1;
 
-    // #TEST#: CMVD12 Create multi-variable declaration
+    // #TEST#: CMVD12 Create multi-variable declaration from next two lines
     Jumble &j5 = j1;
     Jumble **j6;
     j6 = &j4;
