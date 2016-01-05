a6070f5f22d990f0ec971595c8ab06018db53465
Richard Thomson <rthomson@daz3d.com>
2016-01-05 13:06:19 -0700

    Add more test cases for Reverse Conditional

--- RefactorTest/ReverseConditional.cpp
+++ RefactorTest/ReverseConditional.cpp
@@ -19,10 +19,13 @@ namespace ReverseConditionalNamespace
         // #TEST#: RC1 Reverse Conditional
         if (i != 6)
         {
+            // #TEST#: RC2 Reverse Conditional
             if (i != 4)
             {
+                // #TEST#: RC3 Reverse Conditional
                 if (i != 2)
                 {
+                    // #TEST#: RC4 Reverse Conditional
                     if (i != 1)
                     {
                         j = 7;
@@ -44,15 +47,85 @@ namespace ReverseConditionalNamespace
         }
         return j;
     }
+
+    int Test2(int i)
+    {
+        // #TEST#: RC5 Reverse Conditional
+        if (i < 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    int Test3(int i)
+    {
+        // #TEST#: RC6 Reverse Conditional
+        if (i <= 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    int Test4(int i)
+    {
+        // #TEST#: RC7 Reverse Conditional
+        if (0 < i)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    int Test5(int i)
+    {
+        // #TEST#: RC8 Reverse Conditional
+        if (i == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
 }
 
 void TestReverseConditional()
 {
-    REQUIRE_EQUAL(2, ReverseConditionalNamespace::Test1());
-    REQUIRE_EQUAL(3, ReverseConditionalNamespace::Test1());
-    REQUIRE_EQUAL(7, ReverseConditionalNamespace::Test1());
-    REQUIRE_EQUAL(5, ReverseConditionalNamespace::Test1());
-    REQUIRE_EQUAL(7, ReverseConditionalNamespace::Test1());
-    REQUIRE_EQUAL(0, ReverseConditionalNamespace::Test1());
-    REQUIRE_EQUAL(7, ReverseConditionalNamespace::Test1());
+    using namespace ReverseConditionalNamespace;
+
+    REQUIRE_EQUAL(2, Test1());
+    REQUIRE_EQUAL(3, Test1());
+    REQUIRE_EQUAL(7, Test1());
+    REQUIRE_EQUAL(5, Test1());
+    REQUIRE_EQUAL(7, Test1());
+    REQUIRE_EQUAL(0, Test1());
+    REQUIRE_EQUAL(7, Test1());
+
+    REQUIRE_EQUAL(1, Test2(-1));
+    REQUIRE_EQUAL(0, Test2(0));
+    REQUIRE_EQUAL(0, Test2(1));
+
+    REQUIRE_EQUAL(1, Test3(-1));
+    REQUIRE_EQUAL(1, Test3(0));
+    REQUIRE_EQUAL(0, Test3(1));
+
+    REQUIRE_EQUAL(0, Test4(-1));
+    REQUIRE_EQUAL(0, Test4(0));
+    REQUIRE_EQUAL(1, Test4(1));
+
+    REQUIRE_EQUAL(0, Test5(-1));
+    REQUIRE_EQUAL(1, Test5(0));
+    REQUIRE_EQUAL(0, Test5(1));
 }
