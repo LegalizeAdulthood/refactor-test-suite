bde5fdc2f7abc6f466da7aa458746110cf41ef53
Richard <legalize@xmission.com>
2018-02-22 23:13:12 -0700

    Add test case for Rename of identifiers used in static_assert expressions

--- RefactorTest/RenameCpp11.cpp
+++ RefactorTest/RenameCpp11.cpp
@@ -1,6 +1,19 @@
 #include "stdafx.h"
 #include "RenameCpp11.h"
 
+namespace
+{
+
+constexpr bool flag = true;
+constexpr bool flag2 = false;
+
+// #TEST#: R100 Rename flag in static_assert
+static_assert(flag, "Flag is false");
+// #TEST#: R101 Rename flag2 in static_assert
+static_assert(!flag2, "Flag2 is true");
+
+}
+
 void TestRenameCpp11()
 {
 }
