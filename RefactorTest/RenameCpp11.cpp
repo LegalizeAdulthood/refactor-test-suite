be1ff340296821140a79bee6e23d6bac44b9effe
Richard <legalize@xmission.com>
2018-02-22 23:26:16 -0700

    Add test cases for Rename of identifiers in noexcept specifications

--- RefactorTest/RenameCpp11.cpp
+++ RefactorTest/RenameCpp11.cpp
@@ -6,14 +6,28 @@ namespace
 
 constexpr bool flag = true;
 constexpr bool flag2 = false;
+constexpr bool flag3 = true;
+constexpr bool flag4 = false;
 
 // #TEST#: R100 Rename flag in static_assert
 static_assert(flag, "Flag is false");
 // #TEST#: R101 Rename flag2 in static_assert
 static_assert(!flag2, "Flag2 is true");
 
+// #TEST#: R102 Rename flag3 in noexcept specifier
+void f1() noexcept(flag3)
+{
+}
+
+// #TEST#: R103 Rename flag4 in noexcept specifier
+void f2() noexcept(!flag4)
+{
+}
+
 }
 
 void TestRenameCpp11()
 {
+    f1();
+    f2();
 }
