437196a1da26dda8e255320232281af4a40dd617
Richard <legalize@xmission.com>
2018-03-18 22:02:25 -0600

    Fixed: Correct test markers in ExtractParameter.cpp
    Fixes #49

--- RefactorTest/ExtractParameter.cpp
+++ RefactorTest/ExtractParameter.cpp
@@ -34,14 +34,14 @@ void TestMemberPointer()
 {
     // #TEST#: EP1 Extract Parameter ptr
     int (Foo::*ptr)() = &Foo::Operation1;
-    // @TEST#: EP2 Extract Parameter f
+    // #TEST#: EP2 Extract Parameter f
     Foo f;
-    // @TEST#: EP3 Extract Parameter j
-    // @TEST#: EP6 Extract Parameter f
-    // @TEST#: EP4 Extract Parameter ptr
+    // #TEST#: EP3 Extract Parameter j
+    // #TEST#: EP6 Extract Parameter f
+    // #TEST#: EP4 Extract Parameter ptr
     int j = (f.*ptr)();
     require_equal(1, j);
-    // @TEST#: EP5 Extract Parameter ptr
+    // #TEST#: EP5 Extract Parameter ptr
     ptr = &Foo::Operation2;
     j += (f.*ptr)();
     require_equal(3, j);
