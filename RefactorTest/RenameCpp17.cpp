af9ef0bb61b77d7c95e480a17ba17704aa0927c8
Richard <legalize@xmission.com>
2023-12-23 18:21:44 -0700

    Add C++17 structured bindings rename tests

--- RefactorTest/RenameCpp17.cpp
+++ RefactorTest/RenameCpp17.cpp
@@ -2,6 +2,8 @@
 #include "Require.h"
 
 #include <cstdint>
+#include <string>
+#include <tuple>
 
 // C++17 language features:
 // - lambda capture of *this
@@ -111,6 +113,131 @@ void f4()
     REQUIRE_EQUAL(1, RenameCpp17::g_counter);
 }
 
+void f5()
+{
+    // binding to arrays
+    {
+        // #TEST#: R665 Rename variable vals
+        int vals[]{1, 2};
+        // #TEST#: R666 Rename binding cx
+        // #TEST#: R667 Rename binding cy
+        // #TEST#: R668 Rename use of vals
+        const auto [cx, cy] = vals;
+        // #TEST#: R669 Rename use of cx
+        REQUIRE_EQUAL(1, cx);
+        // #TEST#: R670 Rename use of cy
+        REQUIRE_EQUAL(2, cy);
+        // #TEST#: R671 Rename binding x
+        // #TEST#: R672 Rename binding y
+        // #TEST#: R673 Rename use of vals
+        auto [x, y] = vals;
+        // #TEST#: R674 Rename use of x
+        REQUIRE_EQUAL(1, x);
+        // #TEST#: R675 Rename use of y
+        REQUIRE_EQUAL(2, y);
+        // #TEST#: R676 Rename binding vx
+        // #TEST#: R677 Rename binding vy
+        // #TEST#: R678 Rename use of vals
+        auto &[vx, vy] = vals;
+        // #TEST#: R679 Rename use of vx
+        vx = 4;
+        // #TEST#: R680 Rename use of vy
+        vy = 5;
+        REQUIRE_EQUAL(4, vals[0]);
+        REQUIRE_EQUAL(5, vals[1]);
+        // #TEST#: R681 Rename binding crx
+        // #TEST#: R682 Rename binding cry
+        // #TEST#: R683 Rename use of vals
+        const auto &[crx, cry] = vals;
+        // #TEST#: R684 Rename use of crx
+        REQUIRE_EQUAL(4, crx);
+        // #TEST#: R685 Rename use of cry
+        REQUIRE_EQUAL(5, cry);
+    }
+
+    // binding to tuple-like type
+    {
+        using pair = std::tuple<int, std::string>;
+        pair p{1, "one"};
+        // #TEST#: R686 Rename binding val
+        // #TEST#: R687 Rename binding name
+        // #TEST#: R688 Rename use of p
+        const auto [val, name] = p;
+        REQUIRE_EQUAL(1, val);
+        REQUIRE_EQUAL(std::string{"one"}, name);
+        // #TEST#: R689 Rename binding val2
+        // #TEST#: R690 Rename binding name2
+        // #TEST#: R691 Rename use of p
+        auto [val2, name2] = p;
+        // #TEST#: R692 Rename use of val2
+        REQUIRE_EQUAL(1, val2);
+        // #TEST#: R693 Rename use of name2
+        REQUIRE_EQUAL(std::string{"one"}, name2);
+        // #TEST#: R694 Rename binding val3
+        // #TEST#: R695 Rename binding name3
+        // #TEST#: R696 Rename use of p
+        auto &[val3, name3] = p;
+        // #TEST#: R697 Rename use of val3
+        val3 = 11;
+        // #TEST#: R698 Rename use of name3
+        name3 = "eleven";
+        REQUIRE_EQUAL(11, std::get<0>(p));
+        REQUIRE_EQUAL(std::string{"eleven"}, std::get<1>(p));
+        // #TEST#: R699 Rename binding val4
+        // #TEST#: R700 Rename binding name4
+        // #TEST#: R701 Rename use of p
+        const auto &[val4, name4] = p;
+        // #TEST#: R702 Rename use of val4
+        REQUIRE_EQUAL(11, val4);
+        // #TEST#: R703 Rename use of name4
+        REQUIRE_EQUAL(std::string{"eleven"}, name4);
+    }
+
+    // binding to data members
+    {
+        struct Foo
+        {
+            int val;
+            std::string name;
+        };
+        Foo f{1, "one"};
+        // #TEST#: R704 Rename binding val1
+        // #TEST#: R705 Rename binding name1
+        // #TEST#: R706 Rename use of f
+        const auto [val1, name1] = f;
+        // #TEST#: R707 Rename use of val1
+        REQUIRE_EQUAL(1, val1);
+        // #TEST#: R708 Rename use of name1
+        REQUIRE_EQUAL(std::string{"one"}, name1);
+        // #TEST#: R709 Rename binding val2
+        // #TEST#: R710 Rename binding name2
+        // #TEST#: R711 Rename use of f
+        const auto &[val2, name2] = f;
+        // #TEST#: R712 Rename use of val2
+        REQUIRE_EQUAL(1, val2);
+        // #TEST#: R713 Rename use of name2
+        REQUIRE_EQUAL(std::string{"one"}, name2);
+        // #TEST#: R714 Rename binding val3
+        // #TEST#: R715 Rename binding name3
+        // #TEST#: R716 Rename use of f
+        auto &[val3, name3] = f;
+        // #TEST#: R717 Rename use of val3
+        val3 = 11;
+        // #TEST#: R718 Rename use of name3
+        name3 = "eleven";
+        REQUIRE_EQUAL(11, f.val);
+        REQUIRE_EQUAL(std::string{"eleven"}, f.name);
+        // #TEST#: R719 Rename binding val4
+        // #TEST#: R720 Rename binding name4
+        // #TEST#: R721 Rename use of f
+        auto [val4, name4] = f;
+        // #TEST#: R722 Rename use of val4
+        REQUIRE_EQUAL(11, val4);
+        // #TEST#: R723 Rename use of name4
+        REQUIRE_EQUAL(std::string{"eleven"}, name4);
+    }
+}
+
 }    // namespace
 
 void TestRenameCpp17()
@@ -119,4 +246,5 @@ void TestRenameCpp17()
     f2();
     f3();
     f4();
+    f5();
 }
