4d6a1a667c14cd62931a79547bd80c9595a96209
Richard <legalize@xmission.com>
2023-12-26 11:32:16 -0700

    Switch Replace Auto Ptr With Unique Ptr tests to use Require.h
    Switch a run-time assert to a test failure exception.

--- RefactorTest/ReplaceAutoPtrWithUniquePtr.cpp
+++ RefactorTest/ReplaceAutoPtrWithUniquePtr.cpp
@@ -1,4 +1,5 @@
-#include <cassert>
+#include "Require.h"
+
 #include <memory>
 
 static void TestStackAutoPtr()
@@ -6,23 +7,28 @@ static void TestStackAutoPtr()
     // #TEST#: RAWU1 Replace auto_ptr with unique_ptr
     std::auto_ptr<int> i(new int(0));
     *i = 10;
-    assert(*i == 10);
+    REQUIRE_EQUAL(10, *i);
 
     // #TEST#: RAWU2 Replace auto_ptr with unique_ptr
     std::auto_ptr<int> cpy;
     cpy = i;
+    REQUIRE_EQUAL(10, *cpy);
+    REQUIRE_EQUAL(true, nullptr == i.get());
 }
 
 // #TEST#: RAWU3 Replace auto_ptr with unique_ptr
-static void takeOwnership(std::auto_ptr<int> p) { assert(*p == 10); }
+static void takeOwnership(std::auto_ptr<int> p)
+{
+    REQUIRE_EQUAL(10, *p);
+}
 
 static void TestAutoPtrArgument()
 {
     // #TEST#: RAWU4 Replace auto_ptr with unique_ptr
     std::auto_ptr<int> i(new int(10));
-    assert(*i == 10);
+    REQUIRE_EQUAL(10, *i);
     takeOwnership(i);
-    assert(!i.get());
+    REQUIRE_EQUAL(true, nullptr == i.get());
 }
 
 void TestReplaceAutoPtrWithUniquePtr()
