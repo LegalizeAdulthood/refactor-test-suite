da3cb2de3e89bfd58a3f0f3a873201065e085e49
Richard <legalize@xmission.com>
2023-12-27 10:58:05 -0700

    Add Inline Type Alias test cases for typedefs

--- RefactorTest/InlineTypeAlias.cpp
+++ RefactorTest/InlineTypeAlias.cpp
@@ -10,6 +10,11 @@ namespace InlineTypeAlias
 using Int = int;
 using StringVec = std::vector<std::string>;
 
+typedef int IntT;
+typedef std::string StringT;
+// #TEST#: ITA13 Inline typedef StringT
+typedef std::vector<StringT> StringVecT;
+
 }    // namespace InlineTypeAlias
 
 std::ostream &operator<<(std::ostream &str, InlineTypeAlias::StringVec::const_iterator &it)
@@ -44,9 +49,30 @@ void f1()
     REQUIRE_EQUAL(end, begin);
 }
 
+void f2()
+{
+    typedef int IntT;
+    // #TEST#: ITA7 Inline typedef IntT
+    IntT x{10};
+    REQUIRE_EQUAL(10, x);
+
+    // #TEST#: ITA8 Inline typedef InlineTypeAlias::StringVecT
+    InlineTypeAlias::StringVecT v;
+    // #TEST#: ITA9 Inline typedef InlineTypeAlias::StringVecT (not available)
+    using InlineTypeAlias::StringVecT;
+    // #TEST#: ITA10 Inline typedef StringVec
+    const StringVecT::const_iterator begin = v.cbegin();
+    // #TEST#: ITA11 Inline typedef StringVec
+    typedef StringVecT::const_iterator ConstIteratorT;
+    // #TEST#: ITA12 Inline typedef ConstIterator
+    const ConstIteratorT end = v.cend();
+    REQUIRE_EQUAL(end, begin);
+}
+
 }    // namespace
 
 void TestInlineTypeAlias()
 {
     f1();
+    f2();
 }
