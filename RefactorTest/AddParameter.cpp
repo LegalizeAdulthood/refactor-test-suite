492890c44a392603732d5028dd1eaaba2b42a76e
Richard <legalize@xmission.com>
2023-12-27 20:25:44 -0700

    Add more tests for Add Parameter

--- RefactorTest/AddParameter.cpp
+++ RefactorTest/AddParameter.cpp
@@ -2,12 +2,13 @@
 
 // Adds a new parameter to a method declaration and updates all calls accordingly.
 
+// #TEST#: AP149 Add parameter, int goink
 static AddParameterTester *GetOne();
 
 class AddParameterTester2
 {
 public:
-    // #TEST#: AP24 Add parameter
+    // #TEST#: AP24 Add parameter, int goink
     static void AddParameter()
     {
     }
@@ -15,6 +16,7 @@ public:
 
 struct AddParameterStructTester
 {
+    // #TEST#: AP26 Add parameter, int goink
     static void AddParameter()
     {
     }
@@ -22,155 +24,218 @@ struct AddParameterStructTester
 
 void TestAddParameter()
 {
+    // #TEST#: AP86 Add parameter, int *goink
     AddParameterFn();
+    // #TEST#: AP87 Add parameter, int goink
     AddParameterFn(1);
+    // #TEST#: AP88 Add parameter, int goink
     AddParameterFn(0.0f);
     const float arg = 1.0f;
+    // #TEST#: AP89 Add parameter, int goink
     AddParameterFn(arg);
     int data = 2;
     const int &ref = data;
+    // #TEST#: AP90 Add parameter, int goink
     AddParameterFnRef(ref);
+    // #TEST#: AP91 Add parameter before first, int goink
     AddParameterFnDefault();
+    // #TEST#: AP92 Add parameter before first, int goink
     AddParameterFnDefault(1);
 
     AddParameterTester test;
+    // #TEST#: AP93 Add parameter, int *goink
     test.AddParameter();
+    // #TEST#: AP94 Add parameter, int goink
     test.AddParameter2<int>();
+    // #TEST#: AP95 Add parameter, int *goink
     test.AddParameter(1);
+    // #TEST#: AP96 Add parameter, int *goink
     test.AddParameter(ref);
+    // #TEST#: AP97 Add parameter, int goink
     test.AddParameterRef(ref);
+    // #TEST#: AP98 Add parameter before first, int goink
     test.AddParameterDefault();
+    // #TEST#: AP99 Add parameter before first, int goink
     test.AddParameterDefault(1);
 
+    // #TEST#: AP100 Add parameter, int goink
     AddParameterTester::AddParameterStaticInline();
+    // #TEST#: AP101 Add parameter before first, int goink
     AddParameterTester::AddParameterStaticDefaultInline();
+    // #TEST#: AP102 Add parameter before first, int goink
     AddParameterTester::AddParameterStaticDefaultInline(10);
+    // #TEST#: AP103 Add parameter, int goink
     AddParameterTester::Static();
+    // #TEST#: AP104 Add parameter before x, int goink
     AddParameterTester::StaticDefault(0);
+    // #TEST#: AP105 Add parameter before x, int goink
     AddParameterTester::StaticDefault(1, 2);
+    // #TEST#: AP106 Add parameter before x, int goink
     AddParameterTester::StaticTemplate<int>(0);
+    // #TEST#: AP107 Add parameter before x, int goink
     AddParameterTester::StaticTemplateDefault<int>();
+    // #TEST#: AP108 Add parameter before x, int goink
     AddParameterTester::StaticTemplateDefault<int>(1);
+    // #TEST#: AP109 Add parameter, int goink
     AddParameter2<AddParameterTester2>();
 
     AddParameterStruct s;
+    // #TEST#: AP110 Add parameter, int *goink
     s.AddParameter();
+    // #TEST#: AP111 Add parameter, int goink
     s.AddParameter(1);
+    // #TEST#: AP112 Add parameter, int goink
     s.AddParameter2<int>();
+    // #TEST#: AP113 Add parameter, int goink
     s.AddParameterRef(ref);
+    // #TEST#: AP114 Add parameter before first, int goink
+    // #TEST#: AP115 Add parameter after first, int goink = 1
     s.AddParameterDefault();
+    // #TEST#: AP116 Add parameter before first, int goink
+    // #TEST#: AP117 Add parameter after first, int goink = 1
     s.AddParameterDefault(1);
+    // #TEST#: AP118 Add parameter, int goink
     AddParameterStruct::AddParameterStaticInline();
+    // #TEST#: AP119 Add parameter before first, int goink
+    // #TEST#: AP120 Add parameter after first, int goink = 1
     AddParameterStruct::AddParameterStaticDefaultInline();
+    // #TEST#: AP121 Add parameter before first, int goink
+    // #TEST#: AP122 Add parameter after first, int goink = 1
     AddParameterStruct::AddParameterStaticDefaultInline(10);
+    // #TEST#: AP123 Add parameter, int goink
     AddParameterStruct::Static();
+    // #TEST#: AP124 Add parameter before y, int goink
+    // #TEST#: AP125 Add parameter after y, int goink = 3
     AddParameterStruct::StaticDefault(0);
+    // #TEST#: AP126 Add parameter before y, int goink
+    // #TEST#: AP127 Add parameter after y, int goink = 3
     AddParameterStruct::StaticDefault(1, 2);
+    // #TEST#: AP128 Add parameter, int goink
     AddParameterStruct::StaticTemplate<int>(0);
+    // #TEST#: AP129 Add parameter before x, int goink
     AddParameterStruct::StaticTemplateDefault<int>();
+    // #TEST#: AP130 Add parameter before x, int goink
     AddParameterStruct::StaticTemplateDefault<int>(1);
+    // #TEST#: AP131 Add parameter, int goink
     AddParameter2<AddParameterStructTester>();
 
     AddParameterNamespace::Outer::Inner inner;
+    // #TEST#: AP132 Add parameter, int goink
     inner.Operation();
+    // #TEST#: AP133 Add parameter, int goink
     AddParameterNamespace::Outer outer = AddParameterNamespace::Outer();
+    // #TEST#: AP134 Add parameter, int goink
     outer.Operation();
 
     using AddParameterNamespace::Outer;
     Outer outer2;
+    // #TEST#: AP135 Add parameter, int goink
     outer2.Operation();
     namespace Frob = AddParameterNamespace;
     Frob::Outer::Inner inner2;
+    // #TEST#: AP136 Add parameter, int goink
     inner2.Operation();
 
     // default constructor
     AddParameterTester def1;
+    // #TEST#: AP137 Add parameter, int goink
     AddParameterTester def2 = AddParameterTester();
     AddParameterTester *def3 = new AddParameterTester;
     delete def3;
+    // #TEST#: AP138 Add parameter, int goink
     def3 = new AddParameterTester();
     delete def3;
+    // #TEST#: AP139 Add parameter, int goink
     def3 = GetOne();
     delete def3;
 }
 
+// #TEST#: AP140 Add parameter, int goink
 static AddParameterTester *GetOne()
 {
+    // #TEST#: AP141 Add parameter, int goink
     return new AddParameterTester();
 }
 
-// #TEST#: AP13 Add parameter of type double
+// #TEST#: AP13 Add parameter, int *goink
 void AddParameterFn()
 {
 }
 
-// #TEST#: AP14 Add parameter
+// #TEST#: AP14 Add parameter, int goink
 void AddParameterFn(int first)
 {
 }
 
-// #TEST#: AP15 Add parameter
+// #TEST#: AP15 Add parameter, int goink
 void AddParameterFn(float &arg)
 {
     arg = 1.0f;
 }
 
-// #TEST#: AP16 Add parameter
+// #TEST#: AP16 Add parameter, int goink
 void AddParameterFn(const float &arg)
 {
 }
 
-// #TEST#: AP17 Add parameter
+// #TEST#: AP17 Add parameter, int goink
 void AddParameterFnRef(const int &first)
 {
 }
 
-// #TEST#: AP18 Add parameter before first
+// #TEST#: AP18 Add parameter before first, int goink
 void AddParameterFnDefault(int first)
 {
 }
 
-// #TEST#: AP19 Add parameter
+// #TEST#: AP19 Add parameter, int *goink
 void AddParameterTester::AddParameter(int first)
 {
 }
 
-// #TEST#: AP20 Add parameter
+// #TEST#: AP20 Add parameter, int goink
 void AddParameterTester::AddParameterRef(const int &first)
 {
 }
 
-// #TEST#: AP21 Add parameter
+// #TEST#: AP21 Add parameter before first, int goink
 void AddParameterTester::AddParameterDefault(int first)
 {
 }
 
-// #TEST#: AP22 Add parameter
+// #TEST#: AP22 Add parameter, int goink
 void AddParameterTester::Static()
 {
 }
 
-// #TEST#: AP23 Add parameter
+// #TEST#: AP23 Add parameter before x, int goink
 void AddParameterTester::StaticDefault(int x, int y)
 {
 }
 
+// #TEST#: AP142 Add parameter, int goink
 void AddParameterStruct::AddParameter(int first)
 {
 }
 
+// #TEST#: AP143 Add parameter, int goink
 void AddParameterStruct::AddParameterRef(const int &first)
 {
 }
 
+// #TEST#: AP144 Add parameter before first, int goink
+// #TEST#: AP145 Add parameter after first, int goink = 1
 void AddParameterStruct::AddParameterDefault(int first)
 {
 }
 
+// #TEST#: AP146 Add parameter, int goink
 void AddParameterStruct::Static()
 {
 }
 
+// #TEST#: AP147 Add parameter before y, int goink
+// #TEST#: AP148 Add parameter after y, int goink = 3
 void AddParameterStruct::StaticDefault(int x, int y)
 {
 }
--- RefactorTest/AddParameter.h
+++ RefactorTest/AddParameter.h
@@ -1,15 +1,22 @@
 #pragma once
 
+// #TEST#: AP27 Add parameter, int *goink
 extern void AddParameterFn();
+// #TEST#: AP28 Add parameter, int goink
 extern void AddParameterFn(int first);
+// #TEST#: AP29 Add parameter, int goink
 extern void AddParameterFn(float &first);
+// #TEST#: AP30 Add parameter, int goink
 extern void AddParameterFn(const float &first);
+// #TEST#: AP31 Add parameter, int goink
 extern void AddParameterFnRef(const int &first);
+// #TEST#: AP32 Add parameter before first, int goink
 extern void AddParameterFnDefault(int first = 0);
 
 class AddParameterTester
 {
 public:
+    // #TEST#: AP33 Add parameter, int goink
     AddParameterTester()
     {
     }
@@ -17,44 +24,44 @@ public:
     {
     }
 
-    // #TEST#: AP1 Add parameter
+    // #TEST#: AP1 Add parameter, int *goink
     void AddParameter()
     {
     }
 
-    // #TEST#: AP2 Add parameter
+    // #TEST#: AP2 Add parameter, int goink
     template <typename T>
     void AddParameter2()
     {
     }
 
-    // #TEST#: AP3 Add parameter
+    // #TEST#: AP3 Add parameter, int *goink
     void AddParameter(int first);
-    // #TEST#: AP4 Add parameter after first
+    // #TEST#: AP4 Add parameter after first, int goink
     void AddParameterRef(const int &first);
-    // #TEST#: AP5 Add parameter before first
+    // #TEST#: AP5 Add parameter before first, int goink
     void AddParameterDefault(int first = 0);
 
-    // #TEST#: AP6 Add parameter
+    // #TEST#: AP6 Add parameter, int goink
     static void AddParameterStaticInline()
     {
     }
-    // #TEST#: AP7 Add parameter before first
+    // #TEST#: AP7 Add parameter before first, int goink
     static void AddParameterStaticDefaultInline(int first = 0)
     {
     }
-    // #TEST#: AP8 Add parameter
+    // #TEST#: AP8 Add parameter, int goink
     static void Static();
-    // #TEST#: AP9 Add parameter after x
+    // #TEST#: AP9 Add parameter after x, int goink
     static void StaticDefault(int x, int y = 0);
-    // #TEST#: AP10 Add parameter before x
     template <typename T>
+    // #TEST#: AP10 Add parameter before x, int goink
     static void StaticTemplate(int x)
     {
         T val = x;
     }
-    // #TEST#: AP11 Add parameter before x
     template <typename T>
+    // #TEST#: AP11 Add parameter before x, int goink
     static void StaticTemplateDefault(int x = 0)
     {
     }
@@ -64,39 +71,45 @@ template <typename T>
 class AddParameterTemplateClass
 {
 public:
+    // #TEST#: AP34 Add parameter, int goink
     static T StaticTemplate()
     {
         return T(0);
     }
 
+    // #TEST#: AP35 Add parameter, int goink
     T Template()
     {
         return T(0);
     }
 
+    // #TEST#: AP36 Add parameter, int goink
     static void StaticTemplateArg(T arg)
     {
     }
 
+    // #TEST#: AP37 Add parameter, int goink
     void TemplateArg(T arg)
     {
     }
 
+    // #TEST#: AP38 Add parameter before arg, int goink
     void TemplateArgDefault(T arg = 0)
     {
     }
 };
 
-// #TEST#: AP12 Add parameter
 template <typename T>
+// #TEST#: AP12 Add parameter, int goink
 void AddParameter2()
 {
-    // #TEST#: AP25 Add parameter
+    // #TEST#: AP25 Add parameter, int goink
     T::AddParameter();
 }
 
 struct AddParameterStruct
 {
+    // #TEST#: AP39 Add parameter, int goink
     AddParameterStruct()
     {
     }
@@ -104,33 +117,47 @@ struct AddParameterStruct
     {
     }
 
+    // #TEST#: AP40 Add parameter, int *goink
     void AddParameter()
     {
     }
 
     template <typename T>
+    // #TEST#: AP41 Add parameter, int goink
     void AddParameter2()
     {
     }
 
+    // #TEST#: AP42 Add parameter, int goink
     void AddParameter(int first);
+    // #TEST#: AP43 Add parameter, int goink
     void AddParameterRef(const int &first);
+    // #TEST#: AP44 Add parameter before first, int goink
+    // #TEST#: AP45 Add parameter after first, int goink = 1
     void AddParameterDefault(int first = 0);
 
+    // #TEST#: AP46 Add parameter, int goink
     static void AddParameterStaticInline()
     {
     }
+    // #TEST#: AP47 Add parameter before first, int goink
+    // #TEST#: AP48 Add parameter after first, int goink = 1
     static void AddParameterStaticDefaultInline(int first = 0)
     {
     }
+    // #TEST#: AP49 Add parameter, int goink
     static void Static();
+    // #TEST#: AP50 Add parameter before y, int goink
+    // #TEST#: AP51 Add parameter after y, int goink = 3
     static void StaticDefault(int x, int y = 0);
     template <typename T>
+    // #TEST#: AP52 Add parameter, int goink
     static void StaticTemplate(int x)
     {
         T val = x;
     }
     template <typename T>
+    // #TEST#: AP53 Add parameter before x, int goink
     static void StaticTemplateDefault(int x = 0)
     {
     }
@@ -139,6 +166,7 @@ struct AddParameterStruct
 union AddParameterUnion
 {
 public:
+    // #TEST#: AP54 Add parameter, int goink
     AddParameterUnion()
     {
     }
@@ -146,33 +174,46 @@ public:
     {
     }
 
+    // #TEST#: AP55 Add parameter, int *goink
     void AddParameter()
     {
     }
 
     template <typename T>
+    // #TEST#: AP56 Add parameter, int goink
     void AddParameter2()
     {
     }
 
+    // #TEST#: AP57 Add parameter, int goink
     void AddParameter(int first);
+    // #TEST#: AP58 Add parameter, int goink
     void AddParameterRef(const int &first);
+    // #TEST#: AP59 Add parameter before first, int goink
     void AddParameterDefault(int first = 0);
 
+    // #TEST#: AP60 Add parameter, int goink
     static void AddParameterStaticInline()
     {
     }
+    // #TEST#: AP61 Add parameter before first, int goink
+    // #TEST#: AP62 Add parameter after first, int goink = 1
     static void AddParameterStaticDefaultInline(int first = 0)
     {
     }
+    // #TEST#: AP63 Add parameter, int goink
     static void Static();
+    // #TEST#: AP64 Add parameter before y, int goink
+    // #TEST#: AP65 Add parameter after y, int goink = 1
     static void StaticDefault(int x, int y = 0);
     template <typename T>
+    // #TEST#: AP66 Add parameter, int goink
     static void StaticTemplate(int x)
     {
         T val = x;
     }
     template <typename T>
+    // #TEST#: AP67 Add parameter before x, int goink
     static void StaticTemplateDefault(int x = 0)
     {
     }
@@ -182,6 +223,7 @@ template <typename Other>
 union AddParameterUnionTemplate
 {
 public:
+    // #TEST#: AP68 Add parameter, int goink
     AddParameterUnionTemplate()
     {
     }
@@ -189,41 +231,54 @@ public:
     {
     }
 
+    // #TEST#: AP69 Add parameter, int *goink
     void AddParameter()
     {
     }
 
     template <typename T>
+    // #TEST#: AP70 Add parameter, int goink
     void AddParameter2()
     {
     }
 
+    // #TEST#: AP71 Add parameter, int goink
     void AddParameter(int first);
+    // #TEST#: AP72 Add parameter, int goink
     void AddParameterRef(const int &first);
+    // #TEST#: AP73 Add parameter before first, int goink
     void AddParameterDefault(int first = 0);
 
+    // #TEST#: AP74 Add parameter, int goink
     static void AddParameterStaticInline()
     {
     }
+    // #TEST#: AP75 Add parameter before first, int goink
     static void AddParameterStaticDefaultInline(int first = 0)
     {
     }
+    // #TEST#: AP76 Add parameter, int goink
     static void Static();
+    // #TEST#: AP77 Add parameter before y, int goink
     static void StaticDefault(int x, int y = 0);
     template <typename T>
+    // #TEST#: AP78 Add parameter, int goink
     static void StaticTemplate(int x)
     {
         T val = x;
     }
     template <typename T>
+    // #TEST#: AP79 Add parameter before x, int goink
     static void StaticTemplateDefault(int x = 0)
     {
     }
 
+    // #TEST#: AP80 Add parameter, int goink
     static Other OtherStaticTemplate()
     {
         return Other(0);
     }
+    // #TEST#: AP81 Add parameter, int goink
     Other OtherTemplate()
     {
         return Other(0);
@@ -235,6 +290,7 @@ namespace AddParameterNamespace
 class Outer
 {
 public:
+    // #TEST#: AP82 Add parameter, int goink
     Outer()
     {
     }
@@ -242,6 +298,7 @@ public:
     {
     }
 
+    // #TEST#: AP83 Add parameter, int goink
     void Operation()
     {
     }
@@ -249,10 +306,12 @@ public:
     class Inner
     {
     public:
+        // #TEST#: AP84 Add parameter, int goink
         Inner()
         {
         }
 
+        // #TEST#: AP85 Add parameter, int goink
         void Operation()
         {
         }
