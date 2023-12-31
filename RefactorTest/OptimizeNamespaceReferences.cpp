1ce1d8fd4562a0b961203b56c8df6aae3edecb5b
Richard <legalize@xmission.com>
2023-12-31 15:21:11 -0700

    Rename Optimize Namespace References to Remove Unused Includes
    The original test name was a holdover from ReFactor!Pro, which
    originated as a C# refactoring tool.  Several tools provide a means of
    removing unused includes (ReSharper for C++, IncludeWhatYouUse and
    so-on).

--- RefactorTest/OptimizeNamespaceReferences.cpp
+++
@@ -1,8 +0,0 @@
-// #TEST#: ONR1 Optimize away these headers
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-// Removes unused namespace references (unused #include files in C++).
-
-void TestOptimizeNamespaceReferences() {}
---
+++ RefactorTest/RemoveUnusedIncludes.cpp
@@ -0,0 +1,10 @@
+// #TEST#: RUI1 Remove unused includes
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Removes unused headers
+
+void TestOptimizeNamespaceReferences()
+{
+}
