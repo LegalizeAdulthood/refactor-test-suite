2062f7f1762919138f3e610c1e8083ed641dff48
Richard <legalize@xmission.com>
2024-01-13 13:42:19 -0700

    Update check script to use drop-comments tool

--- results/scripts/check.bat
+++ results/scripts/check.bat
@@ -2,7 +2,7 @@
 setlocal
 set diff=%TEMP%\diff.txt
 cls
-git diff RefactorTest | find /v "index" > %diff%
+git diff RefactorTest | find /v "index" | drop-comments > %diff%
 if not exist results\diffs\%1.txt exit /b 1
 echo Comparing diffs for %1
-find /v "index" < results\diffs\%1.txt | @diff -i -c - %diff%
+find /v "index" < results\diffs\%1.txt | drop-comments | @diff -i -c - %diff%
