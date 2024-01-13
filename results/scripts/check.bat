2020ebcf839a083da245cefe11054fb3ca329e15
Richard <legalize@xmission.com>
2024-01-13 13:28:15 -0700

    Improve check script
    It shouldn't drop lines with // because it might be a comment after a
    line of code.

--- results/scripts/check.bat
+++ results/scripts/check.bat
@@ -1,5 +1,8 @@
-@cls
-@git diff RefactorTest | find /v "index" | find /v "//" > c:\tmp\diff.txt
-@if not exist results\diffs\%1.txt exit /b 1
-@echo Comparing diffs for %1
-@find /v "index" < results\diffs\%1.txt | find /v "//" | @diff -i -c - c:\tmp\diff.txt
+@echo off
+setlocal
+set diff=%TEMP%\diff.txt
+cls
+git diff RefactorTest | find /v "index" > %diff%
+if not exist results\diffs\%1.txt exit /b 1
+echo Comparing diffs for %1
+find /v "index" < results\diffs\%1.txt | @diff -i -c - %diff%
