a69669e4a4dfcf64a5c51bbbbdc5c720d2f89bf1
Richard <legalize@xmission.com>
2024-01-18 20:08:23 -0700

    Renumber Inline Type Alias tests

--- RefactorTest/InlineTypeAlias.cpp
+++ RefactorTest/InlineTypeAlias.cpp
@@ -12,7 +12,7 @@ using StringVec = std::vector<std::string>;
 
 typedef int IntT;
 typedef std::string StringT;
-// #TEST#: ITA13 Inline typedef StringT
+// #TEST#: ITA1 Inline typedef StringT
 typedef std::vector<StringT> StringVecT;
 
 }    // namespace InlineTypeAlias
@@ -32,19 +32,19 @@ namespace
 void f1()
 {
     using Int = int;
-    // #TEST#: ITA1 Inline type alias Int
+    // #TEST#: ITA2 Inline type alias Int
     Int x{10};
     REQUIRE_EQUAL(10, x);
 
-    // #TEST#: ITA2 Inline type alias InlineTypeAlias::StringVec
+    // #TEST#: ITA3 Inline type alias InlineTypeAlias::StringVec
     InlineTypeAlias::StringVec v;
-    // #TEST#: ITA3 Inline type alias InlineTypeAlias::StringVec (not available)
+    // #TEST#: ITA4 Inline type alias InlineTypeAlias::StringVec (not available)
     using InlineTypeAlias::StringVec;
-    // #TEST#: ITA4 Inline type alias StringVec
-    const StringVec::const_iterator begin = v.cbegin();
     // #TEST#: ITA5 Inline type alias StringVec
+    const StringVec::const_iterator begin = v.cbegin();
+    // #TEST#: ITA6 Inline type alias StringVec
     using ConstIterator = StringVec::const_iterator;
-    // #TEST#: ITA6 Inline type alias ConstIterator
+    // #TEST#: ITA7 Inline type alias ConstIterator
     const ConstIterator end = v.cend();
     REQUIRE_EQUAL(end, begin);
 }
@@ -52,19 +52,19 @@ void f1()
 void f2()
 {
     typedef int IntT;
-    // #TEST#: ITA7 Inline typedef IntT
+    // #TEST#: ITA8 Inline typedef IntT
     IntT x{10};
     REQUIRE_EQUAL(10, x);
 
-    // #TEST#: ITA8 Inline typedef InlineTypeAlias::StringVecT
+    // #TEST#: ITA9 Inline typedef InlineTypeAlias::StringVecT
     InlineTypeAlias::StringVecT v;
-    // #TEST#: ITA9 Inline typedef InlineTypeAlias::StringVecT (not available)
+    // #TEST#: ITA10 Inline typedef InlineTypeAlias::StringVecT (not available)
     using InlineTypeAlias::StringVecT;
-    // #TEST#: ITA10 Inline typedef StringVec
-    const StringVecT::const_iterator begin = v.cbegin();
     // #TEST#: ITA11 Inline typedef StringVec
+    const StringVecT::const_iterator begin = v.cbegin();
+    // #TEST#: ITA12 Inline typedef StringVec
     typedef StringVecT::const_iterator ConstIteratorT;
-    // #TEST#: ITA12 Inline typedef ConstIterator
+    // #TEST#: ITA13 Inline typedef ConstIterator
     const ConstIteratorT end = v.cend();
     REQUIRE_EQUAL(end, begin);
 }
