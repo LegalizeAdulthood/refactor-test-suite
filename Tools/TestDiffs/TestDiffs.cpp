866e1754722ed0ba0dce16dfcde4a4b83bec8777
Richard <legalize@xmission.com>
2024-01-20 20:51:59 -0700

    test-diffs checks that aliases have the same diff contents

--- Tools/TestDiffs/TestDiffs.cpp
+++ Tools/TestDiffs/TestDiffs.cpp
@@ -1,10 +1,9 @@
+#include <FileContents.h>
 #include <TestCases.h>
 
 #include <algorithm>
 #include <filesystem>
-#include <fstream>
 #include <iostream>
-#include <map>
 #include <stdexcept>
 #include <string>
 #include <string_view>
@@ -14,24 +13,73 @@
 namespace
 {
 
-std::vector<std::string> g_diffs;
-
-void scanDiffDirectory(std::filesystem::path dir)
+class TestDiffs
 {
-    for (auto &entry : std::filesystem::directory_iterator(dir))
+public:
+    TestDiffs(std::filesystem::path testCaseDir, std::filesystem::path diffDir);
+    void checkDiffs();
+
+private:
+    void scanDiffDirectory();
+
+    std::filesystem::path m_diffDir;
+    std::vector<std::string> m_diffNames;
+};
+
+TestDiffs::TestDiffs(std::filesystem::path testCaseDir, std::filesystem::path diffDir) :
+    m_diffDir(diffDir)
+{
+    if (const std::vector<std::string> errors = testCases::Test::scanTestDirectory(testCaseDir); !errors.empty())
     {
-        if (is_directory(entry))
+        for (const std::string &message : errors)
         {
-            scanDiffDirectory(entry);
+            std::cerr << "error: " << message << '\n';
         }
-        else
+        throw std::runtime_error("Test cases have errors");
+    }
+    scanDiffDirectory();
+}
+
+void TestDiffs::scanDiffDirectory()
+{
+    const auto isTestCaseDiff = [](const std::filesystem::path &path)
+    {
+        if (path.extension().string() != ".txt")
         {
-            g_diffs.emplace_back(entry.path().filename().string());
+            return false;
         }
+        const std::string text{path.stem().string()};
+        std::size_t pos = text.find_first_of("0123456789");
+        return pos != std::string::npos;
+    };
+    for (auto &entry : std::filesystem::directory_iterator(m_diffDir))
+    {
+        if (!isTestCaseDiff(entry))
+        {
+            continue;
+        }
+        m_diffNames.emplace_back(entry.path().stem().string());
     }
 }
 
-void checkDiffs(std::ostream &out)
+bool diffsEqual(const testCases::FileContents &master, const testCases::FileContents &alias)
+{
+    if (master.getLines().size() != alias.getLines().size())
+    {
+        return false;
+    }
+
+    for (std::size_t line = 0; line < master.getLines().size(); ++line)
+    {
+        if (master.getLines()[line] != alias.getLines()[line])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void TestDiffs::checkDiffs()
 {
     for (const testCases::Test &test : testCases::getTests())
     {
@@ -42,9 +90,24 @@ void checkDiffs(std::ostream &out)
 
         for (const std::string &testCase : test.getCases())
         {
-            if (std::find(g_diffs.begin(), g_diffs.end(), testCase + ".txt") == g_diffs.end())
+            if (std::find(m_diffNames.begin(), m_diffNames.end(), testCase) == m_diffNames.end())
             {
-                out << "warning: Test case " << testCase << " has no diff.\n";
+                std::cout << "warning: Test case " << testCase << " has no diff.\n";
+            }
+        }
+
+        for (const testCases::TestCaseAlias &alias : test.getCaseAliases())
+        {
+            testCases::FileContents masterContents(m_diffDir / (alias.master + ".txt"));
+
+            for (const std::string &aliasName : alias.aliases)
+            {
+                testCases::FileContents aliasContents(m_diffDir / (aliasName + ".txt"));
+                if (!diffsEqual(masterContents, aliasContents))
+                {
+                    std::cout << "warning: Test case " << aliasName << " is marked as an alias of " << alias.master
+                              << ", but has a different diff.\n";
+                }
             }
         }
     }
@@ -65,9 +128,10 @@ int main(const std::vector<std::string_view> &args)
             return usage(args[0]);
         }
 
-        testCases::Test::scanTestDirectory(args[1]);
-        scanDiffDirectory(args[2]);
-        checkDiffs(std::cout);
+        const std::string_view testCaseDir{args[1]};
+        const std::string_view diffDir{args[2]};
+        TestDiffs tool(testCaseDir, diffDir);
+        tool.checkDiffs();
         return 0;
     }
     catch (const std::exception &bang)
