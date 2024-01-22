081b1779fe313fd1e59ca9120e6c71c7c304a628
Richard <legalize@xmission.com>
2024-01-22 08:30:04 -0700

    Update regen-summary script

--- results/scripts/regen-summary.bat
+++ results/scripts/regen-summary.bat
@@ -1,2 +1,5 @@
+@rem
+@rem Regenerate the summary results files.
+@rem
 tool-summary --annotate RefactorTest results > SummaryResults.md
 tool-summary --annotate RefactorTest results\preliminary > PreliminaryResults.md
