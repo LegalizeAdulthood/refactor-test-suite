a2da4fafb916c742c301d4c1e09cfe3efbddcd1b
Richard <legalize@xmission.com>
2024-01-28 14:20:26 -0700

    Add integration test for add-test-alias

---
+++ Tools/Tests/add-test-alias/Expected/Sources/TestRename.cpp
@@ -0,0 +1,53 @@
+#include "Rename.h"
+
+#include "Require.h"
+
+#include <stdexcept>
+#include <string>
+
+// Renames a local variable, function, method, field, property, parameter,
+// type, namespace, active CSS style, C++ global variable or C++ macro and
+// updates all references to the modified element.
+
+// #TEST#: R1 Rename this macro
+#define MACRO_NO_ARGS 3.14159f
+
+// #TEST#: R2 Rename the macro argument x_ from declaration
+// #TEST#: R3 =R2 Rename the macro argument x_ from usage
+// #TEST#: R4 Rename this macro
+#define MACRO_ONE_ARG(x_) (2.0f * x_)
+
+class Operations
+{
+public:
+    int Operation1()
+    {
+        return 1;
+    }
+    int Operation2()
+    {
+        return 2;
+    }
+};
+
+// #TEST#: R5 Rename this class, but class in Rename2.cpp shouldn't be renamed
+class Rename1
+{
+public:
+    // #TEST#: R6 Rename this class via its c'tor
+    Rename1() : x()
+    {
+    }
+
+    ~Rename1()
+    {
+    }
+
+    typedef Rename1 MyClass;
+
+    void Operation()
+    {
+    }
+
+    int x;
+};
---
+++ Tools/Tests/add-test-alias/Inputs/Results/CLionResults.md
@@ -0,0 +1,22 @@
+# CLion
+
+## Add Block Delimiter
+Case | Result
+---- | ------
+ABD1 |  Pass
+ABD2 |  Pass
+ABD3 |  Pass
+ABD4 |  Pass
+ABD5 |  Pass
+ABD6 |  Pass
+ABD7 |  Pass
+
+## Rename
+Case | Result
+---- | ------
+R1  | Pass
+R2  | Pass
+R3  | Pass
+R4  | Pass
+R5   | Failure (selects unrelated names) [1938](https://youtrack.jetbrains.com/issue/CPP-1938)
+R6   | Failure (selects unrelated names) [1938](https://youtrack.jetbrains.com/issue/CPP-1938)
